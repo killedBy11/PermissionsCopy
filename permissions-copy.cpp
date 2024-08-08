2a282b90b8f9befe0839cdf34714f5f9ef3ad3d0
Antoniu Ficard <39056361+killedBy11@users.noreply.github.com>
2024-08-08 12:35:53 +0300

    Update permissions-copy.cpp

--- permissions-copy.cpp
+++ permissions-copy.cpp
@@ -2,15 +2,17 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>
+#include <algorithm>
+#include <locale>
 
-std::string parse_line(const std::string& line, const std::string& sourceSID, const std::string& destinationSID) {
-    // std::cout << line << "\n:" << sourceSID << ":" << sourceSID.length() << ":" << std::endl;
-    if (line.find(sourceSID) == std::string::npos) {
+
+std::string parse_line(std::string line, const std::string& sourceSID, const std::string& destinationSID, bool overwrite) {
+    if (line.find(sourceSID) == std::string::npos || (!overwrite && line.find(destinationSID) != std::string::npos)) {
         return line;
     }
 
     std::string right = line;
-    std::string the_new = "";
+    std::string the_new;
     while (right.length() > 1) {
         size_t op = right.find('(');
         size_t cp = right.find(')');
@@ -21,13 +23,22 @@ std::string parse_line(const std::string& line, const std::string& sourceSID, co
         }
 
         std::string current = right.substr(op, cp - op + 1);
+
+        size_t pos;
+
+        if (overwrite && std::string::npos != current.find(destinationSID)) {
+            pos = line.find(current);
+            line.replace(pos, current.length(), "");
+            goto JMP;
+        }
         
-        size_t pos = current.find(sourceSID);
+        pos = current.find(sourceSID);
         if (pos != std::string::npos) {
-            the_new = current;
-            the_new.replace(pos, sourceSID.length(), destinationSID);
+            current.replace(pos, sourceSID.length(), destinationSID);
+            the_new += current;
         }
 
+        JMP:
         try {
         right = right.substr(cp + 1);
         } catch(std::exception e) {
@@ -41,15 +52,30 @@ std::string parse_line(const std::string& line, const std::string& sourceSID, co
 
 int main() {
     std::string inputFilename;
+    bool overwrite = false;
+
+    std::string overwriteInput;
+
+    std::cout << "In case of conflicting permissions(i.e. the destination user already has permissions on a certain folder/file), do you prefer to OVERWRITE existing permissions or do you want to KEEP existing permissions and disregard the copied permissions: [O/K]";
+    std::cin >> overwriteInput;
+
+    if (overwriteInput[0] != 'O' && overwriteInput[0] != 'K' && overwriteInput[0] != 'o' && overwriteInput[0] != 'k') {
+        return -1;
+    }
+
+    if (overwriteInput[0] == 'O' || overwriteInput[0] == 'o') {
+        overwrite = true;
+    }
+
 
     std::cout << "Input file path:";
-    std::getline(std::cin, inputFilename);
+    std::getline(std::cin >> std::ws, inputFilename);
 
-    std::string outPutFilename;
+    std::string outputFilename;
     std::cout << "Output file path:";
-    std::getline(std::cin, outPutFilename);
+    std::getline(std::cin, outputFilename);
 
-    std::ifstream inputFile(inputFilename);
+    std::ifstream inputFile(inputFilename, std::ios::binary);
 
     std::cout << "Source object SID: ";
     std::string sourceSID;
@@ -59,14 +85,21 @@ int main() {
     std::string destinationSID;
     std::getline(std::cin, destinationSID);
 
-    std::ofstream outputFile(outPutFilename);
+    std::ofstream outputFile(outputFilename);
 
     while (!inputFile.eof()) {
         std::string line;
         std::getline(inputFile, line);
-        std::string result = parse_line(line, sourceSID, destinationSID);
-        outputFile << result << std::endl;
+        
+        line.erase(std::remove_if(line.begin(),line.end(), [](char c) {return c == 0 || c == 13 || c == 10;}), line.end()); 
+
+        std::string result = parse_line(line, sourceSID, destinationSID, overwrite);
+        for (const char c : result) {
+            outputFile << c;
+        }
+        outputFile << std::endl;
     }
+    outputFile.close();
     system("pause");
     return 0;
 }
